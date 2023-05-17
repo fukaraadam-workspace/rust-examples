4fd539b5e5e28a0a300efe2b342d2cc554ddbfe5
Furkan Alan <furkanalan@hotmail.com>
2023-05-17 21:23:09 +0300

    moved to new file to empty main function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+mod some_func;
 mod array;
 
 fn main() {
     array::array_main();
-    println!("Hello, world!");
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    some_func::some_func();
 }
