e8a62087f89a8eb4f32b208310deea14f921cdb0
Furkan Alan <furkanalan@hotmail.com>
2023-06-01 12:13:27 +0300

    simple mod and use statements

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use crate::front_of_house::hosting;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // With use
+    hosting::add_to_waitlist();
 }
